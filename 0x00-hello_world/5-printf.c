cdcfa98338fc8be8ed870a8f6ca63d921db704e2
Itu-Jose <itujose653@gmail.com>
2023-02-18 10:28:09 +0200

    Print string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*Print string to standard output*/
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Description: Prints a string using printf()
+*
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
