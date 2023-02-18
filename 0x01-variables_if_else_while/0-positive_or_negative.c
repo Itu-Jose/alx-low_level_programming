fb9495510729020c839d1b68ce1b4dbfae164154
Itu-Jose <itujose653@gmail.com>
2023-02-18 20:17:19 +0200

    Test if number is positive, negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,14 +19,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		puts("is positive");
+		printf("%d is positive", n);
 	}
 	else if (n < 0)
 	{
-		puts("is negative");
+		printf("%d is negative", n);
 	}
 	else
-		puts("is zero");
+		printf("%d is zero", n);
 
 	return (0);
 }
