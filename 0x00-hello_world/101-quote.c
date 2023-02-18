3c7a0bc2c3c09640f7badd16fa58291eb2a7582d
Itu-Jose <itujose653@gmail.com>
2023-02-18 19:12:02 +0200

    Print string to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+/*Print string to standard error*/
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Description: Prints a string using fprintf()
+*
+*Return: Always 1 (success)
+*/
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
