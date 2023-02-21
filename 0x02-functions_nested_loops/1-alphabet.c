6b1783793b5ab7479c29a454e9b59592d1bd6eb9
Itu-Jose <itujose653@gmail.com>
2023-02-21 18:28:53 +0200

    Print the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/*Print the alphabet using _puthcar*/
+#include "main.h"
+
+/**
+ *main - entry point
+ *
+ *Description: print the alphabe in lowercase 
+ *
+ *Return: 0 (success - when _putchar printed)
+ */
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
