c3976392907c5790d232bf932d6550e177313ce8
Itu-Jose <itujose653@gmail.com>
2023-02-21 18:38:50 +0200

    Print alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -15,3 +15,19 @@ int main(void)
 
 	return (0);
 }
+
+/**
+ * print_alphabet - print out the alphabet in lower case
+ *
+ * Return: no return value.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 0; i <= 25; i++)
+	{
+		_putchar((97 + i));
+	}
+	_putchar(10);
+}
