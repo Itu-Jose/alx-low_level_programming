a6df949da5d7e228c1249aea572e306725dcd3f2
Itu-Jose <itujose653@gmail.com>
2023-03-02 18:20:55 +0200

    Remove references to the C standard library

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,21 @@
 #include "main.h"
-#include <string.h>
-#include <stdio.h>
+
+/**
+* str_len - concatenates two strings
+* @str: input string
+* Return: length of str
+*/
+
+int str_len(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+		;
+
+	return (i);
+}
+
 /**
 * _strcat - concatenates two strings
 * @src: string to append
@@ -8,13 +23,14 @@
 * Return: string containing src and dest
 */
 
+
 char *_strcat(char *dest, char *src)
 {
 	int len_dest, len_src, i;
 	char *ptr_str;
 
-	len_dest = strlen(dest);
-	len_src = strlen(src);
+	len_dest = str_len(dest);
+	len_src = str_len(src);
 
 	for (i = 0; i < len_src; i++)
 	{
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,8 @@
 
 int _putchar(char);
 
-char *_strcat(char *dest, char *src);
+char *_strcat(char *, char *);
+
+int str_len(char *);
 
 #endif
