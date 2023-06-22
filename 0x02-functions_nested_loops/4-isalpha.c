b60761b87e3832a77b4574a78dc35d8109dca75b
Itu-Jose <itujose653@gmail.com>
2023-06-22 21:29:17 +0200

    Check if input character is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+/*Check if input character is lowercase*/
+#include "main.h"
+
+/**
+ * _isalpha - check if character is in lower case
+ * @c: input character to test
+ * Return: 1 (if lowercase), 0 (otherwise).
+ */
+int _isalpha(int c)
+{
+
+	int isalpha = 0;
+
+	if ( (65 <= c &&  90 >= c) || (97 <= c && 122 >= c))
+		isalpha = 1;
+
+	return (isalpha);
+}
