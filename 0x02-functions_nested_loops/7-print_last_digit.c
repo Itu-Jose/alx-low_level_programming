f4dfcd5a72db62efb765a5c5fb0b921bf6d631ff
Itu-Jose <itujose653@gmail.com>
2023-06-24 09:56:43 +0200

    Account for int_min

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <limits.h>
 /**
  * print_last_digit - return the last digit of a number
  * @n: input character
@@ -13,8 +13,16 @@ int print_last_digit(int n)
 	int modulus;
 	char mod_ch;
 
-	modulus = abs(n) % 10;
-	mod_ch = 48 + modulus;
+	if (n == INT_MIN)
+	{
+		modulus = abs(n + 1) % 10;
+		mod_ch = 48 + modulus + 1;
+	}
+	else
+	{
+		modulus = abs(n) % 10;
+		mod_ch = 48 + modulus;
+	}
 	_putchar(mod_ch);
 
 	return (modulus);
