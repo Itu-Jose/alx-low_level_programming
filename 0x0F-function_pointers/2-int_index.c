221dd784ea43d49ed4c0eaebb612e8e6aadef1ae
Itu-Jose <itujose653@gmail.com>
2023-07-19 19:29:34 +0200

    Find element in array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+/*Search for integer*/
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * int_index - search for integer
+ * @array: array of elemnts
+ * @size: size of the array
+ * @cmp: function to execute
+ * Return: index of integer (success)
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, ind;
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+		{
+			ind = i;
+			break;
+		}
+	}
+	return (ind);
+}
