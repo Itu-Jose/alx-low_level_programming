1d8bb472841ce7bee77b3f332a8af51a57a33d8e
Itu-Jose <itujose653@gmail.com>
2023-11-15 12:27:15 +0200

    Print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+/*Print a string in reverse followed by a newline*/
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *	_puts_recursion - print string in reverse followed by newline
+ * @s: input character string
+ * Return: no return.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (!(*s == '\0'))
+	{
+		_print_rev_recursion(++s);
+		_putchar(*--s);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,5 +5,7 @@ int _putchar(char);
 
 void _puts_recursion(char *);
 
+void _print_rev_recursion(char *s);
+
 #endif
 
